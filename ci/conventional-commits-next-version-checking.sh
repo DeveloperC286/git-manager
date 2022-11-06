e56e921c10a3285dcc3611c1ca69baaa70ff2f84
DeveloperC <DeveloperC@protonmail.com>
2022-11-06 14:04:11 +0000

    ci: replacing git tag's reverse sort by
    Replacing the usage of the Git tags command's sort by argument `--sort=-committerdate`, which sorts by the committerdate and then reverses it.
    As the preceding `-` to indicate to reverse the order does not work everywhere, e.g. Alpine Linux.
    
    So replacing it by not reversing the order using the preceding `-` and reversing the order using tail so it is more versatile.

--- ci/conventional-commits-next-version-checking.sh
+++ ci/conventional-commits-next-version-checking.sh
@@ -6,6 +6,6 @@ set -o xtrace
 # Get current version.
 current_version=$(cat "VERSION")
 # Get latest tag.
-latest_tag=$(git tag --sort=-committerdate | head -1)
+latest_tag=$(git tag --sort=committerdate | tail -1)
 # Check current vs expected.
 "${CARGO_HOME}/bin/conventional_commits_next_version" --calculation-mode "Batch" --from-reference "${latest_tag}" --from-version "${latest_tag}" --current-version "${current_version}"
